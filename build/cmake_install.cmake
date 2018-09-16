6bf7b8e66a732a82fb945fb817522227c0f9e324
Jason Zhang <jaz998@users.noreply.github.com>
2018-09-16 23:18:45 +1000

    Completed v1
    compiled and ran fine and meet all project rubrics

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/jaszhang/v2-Project/v2-CarND-Extended-Kalman-Filter-Project-master
+# Install script for directory: /home/jaszhang/v2-Project/v2-CarND-Extended-Kalman-Filter-Project
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/jaszhang/v2-Project/v2-CarND-Extended-Kalman-Filter-Project-master/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/jaszhang/v2-Project/v2-CarND-Extended-Kalman-Filter-Project/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -1,4 +1,5 @@
 #include "kalman_filter.h"
+#include <math.h>
 
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
@@ -25,6 +26,10 @@ void KalmanFilter::Predict() {
   TODO:
     * predict the state
   */
+	x_ = F_ * x_;
+	MatrixXd Ft = F_.transpose();
+	P_ = F_ * P_*Ft + Q_;
+
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
@@ -32,6 +37,16 @@ void KalmanFilter::Update(const VectorXd &z) {
   TODO:
     * update the state by using Kalman Filter equations
   */
+	VectorXd y = z - H_ * x_;
+	MatrixXd Ht = H_.transpose();
+	MatrixXd S = H_ * P_*Ht + R_;
+	MatrixXd Si = S.inverse();
+	MatrixXd K = P_ * Ht*Si;
+
+	// New state
+	x_ = x_ + (K*y);
+	MatrixXd I = MatrixXd::Identity(4, 4);
+	P_ = (I - K * H_)*P_;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
@@ -39,4 +54,34 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
   TODO:
     * update the state by using Extended Kalman Filter equations
   */
+	float px = x_[0];
+	float py = x_[1];
+	float vx = x_[2];
+	float vy = x_[3];
+
+	// If rho == 0, skip the update step to avoid dividing by zero.
+	// This is crude but should be fairly robust on our data set.
+	if (px == 0. && py == 0.)
+		return;
+
+
+	VectorXd hofx(3);
+	float rho = sqrt(px*px + py * py);
+	hofx << rho, atan2(py, px), (px*vx + py * vy) / rho;
+
+	// Update the state using Extended Kalman Filter equations
+	VectorXd y = z - hofx;
+	if (y[1] > M_PI)
+		y[1] -= 2.f*M_PI;
+	if (y[1] < -M_PI)
+		y[1] += 2.f*M_PI;
+	MatrixXd Hjt = H_.transpose();
+	MatrixXd S = H_ * P_*Hjt + R_;
+	MatrixXd Si = S.inverse();
+	MatrixXd K = P_ * Hjt*Si;
+
+	// Compute new state
+	x_ = x_ + (K*y);
+	MatrixXd I = MatrixXd::Identity(4, 4);
+	P_ = (I - K * H_)*P_;
 }
